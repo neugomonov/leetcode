7e414cadcfb43969a53244701b713ffd276bea27
neugomonov <neugomonovv@gmail.com>
2022-10-15 21:19:27 +0300

    🤯 solved: romanToInt

--- romanToInt.ts
+++ romanToInt.ts
@@ -1,42 +1,23 @@
 function romanToInt(s: string): number {
   let result: number = 0;
-  for (let i = 0; i < s.length; i++) {
-    switch (s[i]) {
-      case "I":
-        s[i + 1] !== "V" || "X" ? result++ : result;
-        break;
-      case "V":
-        s[i - 1] === "I" ? (result = result + 4) : (result = result + 5);
-        break;
-      case "X":
-        s[i + 1] !== "L" || "C"
-          ? s[i - 1] === "I"
-            ? (result = result + 9)
-            : (result = result + 10)
-          : result;
-        break;
-      case "L":
-        s[i - 1] === "X" ? (result = result + 40) : (result = result + 50);
-        break;
-      case "C":
-        s[i + 1] !== "D" || "M"
-          ? s[i - 1] === "X"
-            ? (result = result + 90)
-            : (result = result + 100)
-          : result;
-        break;
-      case "D":
-        s[i - 1] === "C" ? (result = result + 400) : (result = result + 500);
-        break;
-      case "M":
-        s[i - 1] === "C" ? (result = result + 900) : (result = result + 1000);
-        break;
-      default:
-        result;
-        break;
-    }
+  let map: { [key: string]: number } = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  s = s
+    .replace("IV", "IIII")
+    .replace("IX", "VIIII")
+    .replace("XL", "XXXX")
+    .replace("XC", "LXXXX")
+    .replace("CD", "CCCC")
+    .replace("CM", "DCCCC");
+  for (const c of s) {
+    result += map[c as keyof object];
   }
   return result;
 }
-
-//2216-1000+900-100+90-5+4=2105!=1994
