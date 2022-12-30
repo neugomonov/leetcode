32987166a72deb880b7fae13b8492d6503159546
Valery Neugomonov <neugomonovv@gmail.com>
2022-12-31 01:16:01 +0600

    🤯 solved: twoSum (naming)

--- twoSum.ts
+++ twoSum.ts
@@ -1,13 +1,13 @@
 const twoSum = (nums: number[], target: number): number[] => {
   const map = {};
-  let returnedNums: number[] = [];
+  let indexes: number[] = [];
   for (let i = 0; i < nums.length; i++) {
     const another = target - nums[i];
     if (map[another] > -1) {
-      returnedNums = [map[another], i];
-      return returnedNums;
+      indexes = [map[another], i];
+      return indexes;
     }
     map[nums[i]] = i;
   }
-  return returnedNums;
+  return indexes;
 };
